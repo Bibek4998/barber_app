69e1617b8fe7ea36e6af7b06afbc4662e0f195de
Bibek4998 <bibekjoshi07@gmail.com>
2025-03-07 10:28:15 +0545

    Added a barber tools

--- lib/pages/barber_tutorial/screen/barber_content_screen.dart
+++ lib/pages/barber_tutorial/screen/barber_content_screen.dart
@@ -5,6 +5,20 @@ class BarberContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Barbering Tools',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.blue.shade900,
+            fontWeight: FontWeight.w800,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue.shade100,
+      body: Padding(padding: const EdgeInsets.all(20),
+    )
+    )
   }
 }
