c6a906bed95ef9d34feb39f0bcb1ae29ccd58ffd
Bibek4998 <bibekjoshi07@gmail.com>
2025-03-17 21:19:24 +0545

    Created a grid view in the barbering_tool file

--- lib/pages/barber_tutorial/screen/barbering_tools.dart
+++ lib/pages/barber_tutorial/screen/barbering_tools.dart
@@ -5,6 +5,27 @@ class BarberingTools extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        title: Text('Barbering Tools'),
+        backgroundColor: Colors.red,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [BoxShadow(color: Colors.black, blurRadius: 5)],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
